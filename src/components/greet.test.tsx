06f212598c992939099a2e57313b4fbf93fb7b08
Sathya Vakacharla <sathyavakacharla@192.168.1.112>
2023-05-19 13:03:31 +0100

    describe added, coverage configuration added with threshold

--- src/components/greet.test.tsx
+++ src/components/greet.test.tsx
@@ -1,14 +1,16 @@
 import {render, screen} from '@testing-library/react';
 import {Greet} from "./greet";
 
-test("Greet renders correctly",()=>{
-    render(<Greet />)
-    const txtElement = screen.getByText('Hello');
-    expect(txtElement).toBeInTheDocument();
-})
+describe("Greet",() => {
+    test("renders correctly",()=>{
+        render(<Greet />)
+        const txtElement = screen.getByText('Hello');
+        expect(txtElement).toBeInTheDocument();
+    })
 
-test("Greet renders name",()=>{
-    render(<Greet name="Sathya"/>);
-    const element = screen.getByText('Hello Sathya');
-    expect(element).toBeInTheDocument();
+    test("renders name",()=>{
+        render(<Greet name="Sathya"/>);
+        const element = screen.getByText('Hello Sathya');
+        expect(element).toBeInTheDocument();
+    })
 })
